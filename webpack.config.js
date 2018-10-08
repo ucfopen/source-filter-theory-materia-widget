137e987ee41422898719ff956b5d81d30d6edbf8
Brandon Stull <bstull@ucf.edu>
2018-10-08 15:50:59 -0400

    Cleanup and corrections to package.json. Cleanup to html files. Removed gulp build process, transitioned to webpack build process with MWDK.

---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+// load the reusable legacy webpack config from materia-widget-dev
+let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+
+module.exports = webpackConfig
